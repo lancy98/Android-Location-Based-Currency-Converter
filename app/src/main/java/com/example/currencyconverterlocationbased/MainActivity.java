a20220867adcee193ea1aa6c5305019eb89a9047
Lancy Norbert Fernandes <>
2019-05-02 11:29:51 +1200

    - Initial Commit

---
+++ app/src/main/java/com/example/currencyconverterlocationbased/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.currencyconverterlocationbased;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
