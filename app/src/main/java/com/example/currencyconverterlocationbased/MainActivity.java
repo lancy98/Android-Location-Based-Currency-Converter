a89e6921e766b68a0509ad27379382f8f2783b1d
Lancy Norbert Fernandes <>
2019-05-08 20:17:51 +1200

    - Added number pad to the application.

--- app/src/main/java/com/example/currencyconverterlocationbased/MainActivity.java
+++ app/src/main/java/com/example/currencyconverterlocationbased/MainActivity.java
@@ -12,31 +12,29 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Currency-Converter-TAG";
-    private TextView textView;
+    private static final int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
     }
 
     public void buttonClicked(View view) {
         Intent intent = new Intent(this, CountrySelectionView.class);
-        startActivityForResult(intent, 100);
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch(requestCode) {
-            case (100) : {
+            case (REQUEST_CODE) : {
                 if (resultCode == Activity.RESULT_OK) {
                     Country country = data.getParcelableExtra("Country");
                     String displayText = "Country name is : " + country.name;
                     Log.d(MainActivity.TAG, displayText);
-                    textView.setText(displayText);
                 }
                 break;
             }
